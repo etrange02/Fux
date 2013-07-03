05673ed4db1c8302c8ca1e03a91dd23265a8f23d
Lecoconnier David <david.lecoconnier@free.fr>
2013-07-03 02:01:41 +0200

    Update exe

--- include/version.h
+++ include/version.h
@@ -4,13 +4,8 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-<<<<<<< HEAD
-	static const char DATE[] = "24";
-	static const char MONTH[] = "06";
-=======
 	static const char DATE[] = "02";
 	static const char MONTH[] = "07";
->>>>>>> 61f90bd77a3b75649e64ce7cc6e276e345dc07f6
 	static const char YEAR[] = "2013";
 	static const char UBUNTU_VERSION_STYLE[] =  "13.07";
 	
@@ -21,31 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 5;
-<<<<<<< HEAD
-	static const long BUILD  = 1627;
-	static const long REVISION  = 9005;
+	static const long BUILD  = 1648;
+	static const long REVISION  = 9126;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3260;
-	#define RC_FILEVERSION 2,5,1627,9005
-	#define RC_FILEVERSION_STRING "2, 5, 1627, 9005\0"
-	static const char FULLVERSION_STRING [] = "2.5.1627.9005";
+	static const long BUILDS_COUNT  = 3303;
+	#define RC_FILEVERSION 2,5,1648,9126
+	#define RC_FILEVERSION_STRING "2, 5, 1648, 9126\0"
+	static const char FULLVERSION_STRING [] = "2.5.1648.9126";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 27;
-=======
-	static const long BUILD  = 1646;
-	static const long REVISION  = 9116;
-	
-	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3301;
-	#define RC_FILEVERSION 2,5,1646,9116
-	#define RC_FILEVERSION_STRING "2, 5, 1646, 9116\0"
-	static const char FULLVERSION_STRING [] = "2.5.1646.9116";
-	
-	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 46;
->>>>>>> 61f90bd77a3b75649e64ce7cc6e276e345dc07f6
+	static const long BUILD_HISTORY  = 48;
 	
 
 }
